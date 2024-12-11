7623ee9f96d780f162e62db82d34b7517b06ab2d
mavlon <mavlon@zeroone.uz>
2024-12-11 10:17:35 +0100

    Initial Commit

---
+++ WiseWalker/WiseWalkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiseWalkerApp.swift
+//  WiseWalker
+//
+//  Created by Mavlon Ergashev on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WiseWalkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
