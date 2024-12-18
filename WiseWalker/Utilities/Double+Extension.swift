54414eca2323e7c0eb4c033a0f0c8e68f5ebd584
mavlon <mavlon@zeroone.uz>
2024-12-18 10:45:33 +0100

    ranking design changed

--- WiseWalker/Utilities/Double+Extension.swift
+++ WiseWalker/Utilities/Double+Extension.swift
@@ -12,3 +12,12 @@ extension Double {
         String(format: "%.0f", self)
     }
 }
+
+extension Int {
+    func formatted() -> String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.groupingSeparator = " " // Use space as the thousand separator
+        return formatter.string(from: NSNumber(value: self)) ?? "\(self)"
+    }
+}
--- WiseWalker/Views/RankingView.swift
+++ WiseWalker/Views/RankingView.swift
@@ -11,8 +11,39 @@ struct RankingView: View {
     var body: some View {
         NavigationStack {
             List {
-                ForEach(0..<25) {
-                    Text("Content \($0)")
+                Section("Top today") {
+                    ForEach(0..<10) { index in
+                        HStack {
+                            Image(systemName: index > 2 ? "medal.fill" : "trophy.fill")
+                                .foregroundStyle(
+                                    index == 0 ? .yellow :
+                                        index == 1 ? .gray :
+                                        index == 2 ? .brown : .secondary
+                                )
+                            Text("Random name")
+                            Spacer()
+                            Text("\(Int.random(in: 8000...20000).formatted()) steps")
+                                .foregroundStyle(.indigo)
+                                .bold()
+                        }
+                    }
+                }
+                Section("Top week") {
+                    ForEach(0..<10) { index in
+                        HStack {
+                            Image(systemName: index > 2 ? "medal.fill" : "trophy.fill")
+                                .foregroundStyle(
+                                    index == 0 ? .yellow :
+                                        index == 1 ? .gray :
+                                        index == 2 ? .brown : .secondary
+                                )
+                            Text("Random name")
+                            Spacer()
+                            Text("\(Int.random(in: 40000...140000).formatted()) steps")
+                                .foregroundStyle(.indigo)
+                                .bold()
+                        }
+                    }
                 }
             }
             .navigationTitle("Ranking")
